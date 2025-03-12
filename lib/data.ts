0f7813d8df62e9a1ae12d9a6d8053cc87279f0a8
Mazhar <mazqoty.01@gmail.com>
2025-03-13 02:43:48 +0300

    03-create-home-page-carousel Home Page Image Carousel Created

--- lib/data.ts
+++ lib/data.ts
@@ -33,6 +33,59 @@ const data = {
         href: '/page/help',
       },
     ],
+    //Image Carousel
+    carousels: [
+      // {
+      //   title: 'Most Popular Shoes For Sale',
+      //   buttonCaption: 'Shop Now',
+      //   image: '/images/banner3.jpg',
+      //   url: '/search?category=Shoes',
+      //   isPublished: true,
+      // },
+      // {
+      //   title: 'Best Sellers in T-Shirts',
+      //   buttonCaption: 'Shop Now',
+      //   image: '/images/banner1.jpg',
+      //   url: '/search?category=T-Shirts',
+      //   isPublished: true,
+      // },
+      // {
+      //   title: 'Best Deals on Wrist Watches',
+      //   buttonCaption: 'See More',
+      //   image: '/images/banner2.jpg',
+      //   url: '/search?category=Wrist Watches',
+      //   isPublished: true,
+      // },
+      {
+        title: 'Most Popular in Pottery',
+        buttonCaption: 'Shop Now',
+        image: '/images/banner4.png',
+        url: '/search?category=Pottery',
+        isPublished: true,
+      },
+      {
+        title: 'Best Sellers in Hand Crafts',
+        buttonCaption: 'Shop Now',
+        image: '/images/banner6.png',
+        url: '/search?category=Hand Crafts',
+        isPublished: true,
+      },
+      {
+        title: 'Best Deals in Wools',
+        buttonCaption: 'See More',
+        image: '/images/banner7.png',
+        url: '/search?category=Wools',
+        isPublished: true,
+      },
+      {
+        title: 'Best Deals in Marble Chess Boards',
+        buttonCaption: 'See More',
+        image: '/images/banner9.png',
+        url: '/search?category=Marble Chess Boards',
+        isPublished: true,
+      },
+    ],
+
   }
   
   export default data
