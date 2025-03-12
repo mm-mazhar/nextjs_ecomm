b9b615e7cf3463232c55b24f598009c999b55372
Mazhar <mazqoty.01@gmail.com>
2025-03-12 23:40:22 +0300

    02-create-website-layout

---
+++ components/shared/footer.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { ADDRESS_PHONE, APP_NAME, COPYRIGHT_TEXT } from '@/lib/constants'
+import { ChevronUp } from 'lucide-react'
+import Link from 'next/link'
+import { Button } from '../ui/button'
+
+
+export default function Footer() {
+  const formattedCopyright = COPYRIGHT_TEXT.replace('{APP_NAME}', APP_NAME)
+  
+  return (
+    <footer className='bg-black  text-white underline-link'>
+      <div className='w-full'>
+        <Button
+          variant='ghost'
+          className='bg-gray-800 w-full  rounded-none '
+          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+        >
+          <ChevronUp className='mr-2 h-4 w-4' />
+          Back to top
+        </Button>
+      </div>
+      <div className='p-4'>
+        <div className='flex justify-center  gap-3 text-sm'>
+          <Link href='/page/conditions-of-use'>Conditions of Use</Link>
+          <Link href='/page/privacy-policy'> Privacy Notice</Link>
+          <Link href='/page/help'>Help</Link>
+        </div>
+        <div className='flex justify-center text-sm'>
+          <p>{formattedCopyright}</p>
+        </div>
+        <div className='mt-8 flex justify-center text-sm text-gray-400'>
+          {ADDRESS_PHONE}
+        </div>
+      </div>
+    </footer>
+  )
+}
---
+++ lib/constants.ts
@@ -0,0 +1,9 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'craftsHub'
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more.'
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Ecommerce Website'
+
+export const PRODUCT_CATEGORIES = ['men', 'women', 'kids', 'accessories'] as const
+
+export const COPYRIGHT_TEXT = '© 2000-2025, {APP_NAME}, Inc. or its affiliates'
+
+export const ADDRESS_PHONE ='123, Main Street, Anytown, CA, Zip 12345 | +1 (123) 456-7890'
