a6c7253a96a7edabf423a628d364d3ef1dcfe6d8
Mazhar <mazqoty.01@gmail.com>
2025-04-09 09:07:21 +0300

    15-signin-with-google

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -12,6 +12,10 @@ export async function signInWithCredentials(user: IUserSignIn) {
   return await signIn('credentials', { ...user, redirect: false })
 }
 
+export const SignInWithGoogle = async () => {
+  await signIn('google')
+}
+
 export const SignOut = async () => {
   const redirectTo = await signOut({ redirect: false })
   redirect(redirectTo.redirect)
@@ -37,3 +41,4 @@ export async function registerUser(userSignUp: IUserSignUp) {
     return { success: false, error: formatError(error) }
   }
 }
+
