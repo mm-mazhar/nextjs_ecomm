a9b165705945470e9510969e43638b0563823b32
Mazhar <mazqoty.01@gmail.com>
2025-04-09 21:55:06 +0300

    19-pay-order-by-stripe

--- app/checkout/[id]/page.tsx
+++ app/checkout/[id]/page.tsx
@@ -1,8 +1,7 @@
-import { notFound } from 'next/navigation'
-import React from 'react'
-
 import { auth } from '@/auth'
 import { getOrderById } from '@/lib/actions/order.actions'
+import { notFound } from 'next/navigation'
+import Stripe from 'stripe'
 import PaymentForm from './payment-form'
 
 export const metadata = {
@@ -23,10 +22,22 @@ const CheckoutPaymentPage = async (props: {
 
   const session = await auth()
 
+  let client_secret = null
+if (order.paymentMethod === 'Stripe' && !order.isPaid) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: Math.round(order.totalPrice * 100),
+    currency: 'USD',
+    metadata: { orderId: order._id },
+  })
+  client_secret = paymentIntent.client_secret
+}
+
   return (
     <PaymentForm
       order={order}
       paypalClientId={process.env.PAYPAL_CLIENT_ID || 'sb'}
+      clientSecret = { client_secret }
       isAdmin={session?.user?.role === 'Admin' || false}
     />
   )
