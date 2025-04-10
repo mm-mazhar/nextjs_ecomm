b09d60d7542192050a60d939a03f4708f6a9a28b
Mazhar <mazqoty.01@gmail.com>
2025-04-10 10:44:47 +0300

    26-add-theme-color

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,16 +1,50 @@
-// import Link from 'next/link'
+import { EllipsisVertical } from 'lucide-react'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
 import CartButton from './cart-button'
-// import { User } from 'lucide-react'
 import UserButton from './user-button'
+import ThemeSwitcher from './theme-switcher'
+// import LanguageSwitcher from './language-switcher'
+// import { useTranslations } from 'next-intl'
 
-export default function Menu() {
+const Menu = ({ forAdmin = false }: { forAdmin?: boolean }) => {
+  // const t = useTranslations()
   return (
     <div className='flex justify-end'>
-      <nav className='flex gap-3 w-full'>
+      <nav className='md:flex gap-3 hidden w-full'>
+        {/* <LanguageSwitcher /> */}
+        <ThemeSwitcher />
         <UserButton />
-
-        <CartButton />
+        {forAdmin ? null : <CartButton />}
+      </nav>
+      <nav className='md:hidden'>
+        <Sheet>
+          <SheetTrigger className='align-middle header-button'>
+            <EllipsisVertical className='h-6 w-6' />
+          </SheetTrigger>
+          <SheetContent className='bg-black text-white  flex flex-col items-start  '>
+            <SheetHeader className='w-full'>
+              <div className='flex items-center justify-between '>
+                {/* <SheetTitle className='  '>{t('Header.Site Menu')}</SheetTitle> */}
+                <SheetTitle>Site Menu</SheetTitle>
+                <SheetDescription></SheetDescription>
+              </div>
+            </SheetHeader>
+            {/* <LanguageSwitcher /> */}
+            <ThemeSwitcher />
+            <UserButton />
+            <CartButton />
+          </SheetContent>
+        </Sheet>
       </nav>
     </div>
   )
 }
+
+export default Menu
